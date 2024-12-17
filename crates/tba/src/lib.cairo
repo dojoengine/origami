cbfb30d220e20d24e311219e868320d08fe37e20
PavitraAgarwal21 <agarwalpavitra3000@gmail.com>
2024-12-17 10:40:08 +0530

    add tba

---
+++ crates/tba/src/lib.cairo
@@ -0,0 +1,1 @@
+pub use token_bound_accounts::{registry, interfaces, accountV3, test_helper, components, utils};
--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -5,6 +5,7 @@ scarb --manifest-path crates/map/Scarb.toml build
 scarb --manifest-path crates/random/Scarb.toml build
 scarb --manifest-path crates/rating/Scarb.toml build
 scarb --manifest-path crates/security/Scarb.toml build
+scarb --manifest-path crates/tba/Scarb.toml build
 
 # Sozo dependent crates.
 sozo build --package "origami_token"
